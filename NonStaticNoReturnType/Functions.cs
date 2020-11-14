c05c9984678f482f56d5dbfbff0d13c9eec9f97f
ANUSHA\Admin <anushacherukuri783@gmail.com>
2020-11-14 16:37:06 +0000

    First Class Traning session example

---
+++ NonStaticNoReturnType/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticNoReturnType
+{
+    class Functions
+    {
+        public string s = "Anusha";
+        public int i = 24;
+        public char c = 'C';
+
+        public void Fun()
+        {
+            Console.WriteLine("My name is:" + s);
+            Console.WriteLine("My age is:" + i);
+            Console.WriteLine("My surname is:" + c);
+        }
+    }
+}
---
+++ NonStaticNoReturnType/NonStaticNoReturn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonStaticNoReturnType
+{
+    class NonStaticNoReturn
+    {
+        class StaticNoReturn
+        {
+            static void Main(string[] args)
+            {
+                Functions f = new Functions();
+                f.Fun();
+            }
+        }
+    }
+}
